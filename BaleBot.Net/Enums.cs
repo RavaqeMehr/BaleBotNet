97cd87235553f17452878c386af28ad3439da36b
Moqavem <sina.farahabadi@gmail.com>
2025-04-11 16:23:48 +0330

    Bump version to 1.0.2 and add ChatType enum

--- BaleBot.Net/Enums.cs
+++ BaleBot.Net/Enums.cs
@@ -1,3 +1,5 @@
+using BaleBot.Net.Types;
+
 namespace BaleBot.Net;
 
 public enum ChatAction
@@ -6,14 +8,13 @@ public enum ChatAction
     UploadPhoto,
     UploadVideo,
     UploadVoice,
+}
 
-    // UploadAudio,
-    // UploadDocument,
-    // RecordVideo,
-    // RecordAudio,
-    // RecordVoice,
-    // FindLocation,
-    // CancelLocation
+public enum ChatType
+{
+    Private,
+    Group,
+    Channel
 }
 
 public static class EnumsExtentions
@@ -24,14 +25,15 @@ public static class EnumsExtentions
             ChatAction.UploadPhoto => "upload_photo",
             ChatAction.UploadVideo => "upload_video",
             ChatAction.UploadVoice => "upload_voice",
-            // ChatAction.UploadAudio => "upload_audio",
-            // ChatAction.UploadDocument => "upload_document",
-            // ChatAction.RecordVideo => "record_video",
-            // ChatAction.RecordAudio => "record_audio",
-            // ChatAction.RecordVoice => "record_voice",
-            // ChatAction.FindLocation => "find_location",
-            // ChatAction.CancelLocation => "cancel_location",
-            // ChatAction.Typing => "typing",
             _ => "typing"
+        };
+
+    public static ChatType GetType(this Chat chat) =>
+        chat.Type switch
+        {
+            "private" => ChatType.Private,
+            "group" => ChatType.Group,
+            "channel" => ChatType.Channel,
+            _ => throw new ArgumentOutOfRangeException(nameof(chat.Type), chat.Type, null)
         };
 }
