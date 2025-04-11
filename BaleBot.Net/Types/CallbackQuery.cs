0a30ed33387231f433798299516653737599ba5c
Moqavem <sina.farahabadi@gmail.com>
2025-04-11 15:55:39 +0330

    Bump version to 1.0.1 and update CallbackQuery class to enforce non-null From property

--- BaleBot.Net/Types/CallbackQuery.cs
+++ BaleBot.Net/Types/CallbackQuery.cs
@@ -3,7 +3,7 @@ namespace BaleBot.Net.Types;
 public class CallbackQuery
 {
     public string Id { get; set; } = default!;
-    public User? From { get; set; }
+    public User From { get; set; } = default!;
     public Message? Message { get; set; }
     public string? Data { get; set; }
 }
