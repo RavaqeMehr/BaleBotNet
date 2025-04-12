76ce6641abdc0d4ecc5f7d212a6f04e7e703c208
Moqavem <sina.farahabadi@gmail.com>
2025-04-13 00:39:25 +0330

    Add ValidateMiniAppInitData method for initializing mini app data validation

---
+++ BaleBot.Net/Methods/MiniApp/ValidateMiniAppInitData.cs
@@ -0,0 +1,51 @@
+using System.Security;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.Json;
+using System.Web;
+
+namespace BaleBot.Net.Methods;
+
+public static partial class Methods
+{
+    public static Dictionary<string, string>? ValidateMiniAppInitData(
+        this BaleBotClient bot,
+        string initData
+    )
+    {
+        bool loginWidget = false;
+        // Console.WriteLine($"bot.Token:\n{bot.Token}\n");
+        // Console.WriteLine($"initData:\n{initData}\n");
+
+        var decoded = HttpUtility.UrlDecode(initData ?? "");
+        // Console.WriteLine($"decoded:\n{decoded}\n");
+
+        // var query = HttpUtility.ParseQueryString(decoded ?? "");
+        var query = HttpUtility.ParseQueryString(initData ?? "");
+
+        var fields = query.AllKeys.ToDictionary(key => key!, key => query[key]!);
+
+        if (fields.Remove("hash", out var hash))
+        {
+            // Console.WriteLine($"hash:\n{hash}\n");
+
+            var dataCheckString = string.Join('\n', fields.Select(kvp => $"{kvp.Key}={kvp.Value}"));
+            var secretKey = loginWidget
+                ? SHA256.HashData(Encoding.ASCII.GetBytes(bot.Token))
+                : HMACSHA256.HashData(
+                    Encoding.ASCII.GetBytes("WebAppData"),
+                    Encoding.ASCII.GetBytes(bot.Token)
+                );
+            var computedHash = HMACSHA256.HashData(
+                secretKey,
+                Encoding.UTF8.GetBytes(dataCheckString)
+            );
+
+            // Console.WriteLine($"computedHash:\n{Convert.ToHexString(computedHash)}\n");
+
+            if (computedHash.SequenceEqual(Convert.FromHexString(hash)))
+                return fields;
+        }
+        throw new SecurityException("Invalid data hash");
+    }
+}
