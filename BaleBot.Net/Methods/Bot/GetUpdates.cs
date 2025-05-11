8977ec1bad80f8f29193e00d521f5ce360b8f881
Moqavem <sina.farahabadi@gmail.com>
2025-05-11 10:40:24 +0330

    feat: add timout for GetUpdates method

--- BaleBot.Net/Methods/Bot/GetUpdates.cs
+++ BaleBot.Net/Methods/Bot/GetUpdates.cs
@@ -4,10 +4,22 @@ namespace BaleBot.Net.Methods;
 
 public static partial class Methods
 {
+    /// <param name="timeout">seconds</param>
     public static async Task<Update[]> GetUpdates(
         this BaleBotClient bot,
         int? offset = null,
-        int? limit = null
+        int? limit = null,
+        int? timeout = null
     ) =>
-        await bot.SendRequest<Update[]>(BotRequest.CreatePost("getUpdates", new { offset, limit }));
+        await bot.SendRequest<Update[]>(
+            BotRequest.CreatePost(
+                "getUpdates",
+                new
+                {
+                    offset,
+                    limit,
+                    timeout
+                }
+            )
+        );
 }
