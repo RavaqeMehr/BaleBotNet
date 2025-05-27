6eb1534e5688f0f617d17d1d5399b0f593cdae3a
Moqavem <sina.farahabadi@gmail.com>
2025-05-27 12:42:44 +0330

    fix: fill invoice type

--- BaleBotNet/Types/Invoice.cs
+++ BaleBotNet/Types/Invoice.cs
@@ -1,3 +1,10 @@
 namespace BaleBotNet.Types;
 
-public class Invoice { }
+public class Invoice
+{
+    public string Title { get; set; } = default!;
+    public string Description { get; set; } = default!;
+    public string StartParameter { get; set; } = default!;
+    public string Currency { get; set; } = default!;
+    public int TotalAmount { get; set; }
+}
