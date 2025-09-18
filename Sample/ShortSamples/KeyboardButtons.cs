003d06a2ef3847b7f2ec1f9b8cd5926e991ff2ed
Moqavem <sina.farahabadi@gmail.com>
2025-09-18 10:21:21 +0330

    add sample for #16

--- Sample/ShortSamples/KeyboardButtons.cs
+++ Sample/ShortSamples/KeyboardButtons.cs
@@ -7,18 +7,22 @@ public partial class ShortSamples
 {
     public async Task KeyboardButtons(long chatId)
     {
+        var replyMarkup = ReplyKeyboard.CreateKeyboard(
+            [
+                [ // چند دکمه در هر سطر
+                    new() { Text = "Option 1" },
+                    new() { Text = "Option 2" }
+                ],
+                [new() { Text = "Cancel ❌" }]
+            ]
+        );
+
         var message = await bot.SendMessage(
             chatId: chatId,
             text: "Choose an option:",
-            replyMarkup: ReplyKeyboard.CreateKeyboard(
-                [
-                    [ // چند دکمه در هر سطر
-                        new() { Text = "Option 1" },
-                        new() { Text = "Option 2" }
-                    ],
-                    [new() { Text = "Cancel ❌" }]
-                ]
-            )
+            replyMarkup: replyMarkup
         );
+
+        var saveChatMarkup = new { chatId, replyMarkup }; // can save chat replyMarkup if needed
     }
 }
