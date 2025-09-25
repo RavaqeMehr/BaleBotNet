90005017c652aa1a7ce65190fda6b8396c2c55db
Moqavem <sina.farahabadi@gmail.com>
2025-09-25 13:38:42 +0330

    add single file sample

---
+++ SingleFileSamples/EchoBot.cs
@@ -0,0 +1,30 @@
+#:property PublishAot=false
+#:package BaleBotNet@2.1.1
+
+using BaleBotNet;
+using BaleBotNet.Enums;
+using BaleBotNet.Methods;
+
+var bot = new BaleBotClient("bot-token");
+int? offset = null;
+
+do
+{
+    var updates = await bot.GetUpdates(offset, 10);
+
+    var tasks = updates?
+        .Where(update => update.Message?.Chat.Type == ChatType.Private)
+        .Select(update => update.Message!)
+        .Select((message) => message.CopyTo(bot, message.Chat.Id))
+        .ToList();
+
+    if (tasks != null && tasks.Count > 0)
+        await Task.WhenAll(tasks);
+
+    if (updates?.Length > 0)
+        offset = updates?.Last().UpdateId + 1;
+
+    await Task.Delay(1000);
+
+} while (true);
+
