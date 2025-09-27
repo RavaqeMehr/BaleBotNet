05c4caa4376fb33a419b71a9df4c962a85eb0455
Moqavem <sina.farahabadi@gmail.com>
2025-09-27 21:24:50 +0330

    feat: add missing chatActionTypes

--- BaleBotNet/Enums/ChatAction.cs
+++ BaleBotNet/Enums/ChatAction.cs
@@ -4,8 +4,11 @@ public enum ChatAction
 {
     Typing,
     UploadPhoto,
+    RecordVideo,
     UploadVideo,
+    RecordVoice,
     UploadVoice,
+    ChooseSticker,
 }
 
 public static class ChatActionExtentions
@@ -14,8 +17,11 @@ public static class ChatActionExtentions
         action switch
         {
             ChatAction.UploadPhoto => "upload_photo",
+            ChatAction.RecordVideo => "record_video",
             ChatAction.UploadVideo => "upload_video",
+            ChatAction.RecordVoice => "record_voice",
             ChatAction.UploadVoice => "upload_voice",
+            ChatAction.ChooseSticker => "choose_sticker",
             _ => "typing"
         };
 }
